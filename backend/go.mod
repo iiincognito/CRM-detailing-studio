9c1eea40b001e7e8cfcfd45e275cc6983112b480
Almaz Akhmetshin <stron_0122@mail.ru>
2026-02-11 20:35:14 +0300

    started

--- backend/go.mod
+++ backend/go.mod
@@ -1,14 +1,7 @@
-module userServiseAPI
+module github.com/iiincognito/CRM-detailing-studio/backend
 
 go 1.25.5
 
-require (
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.8.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/julienschmidt/httprouter v1.3.0 // indirect
-	github.com/lib/pq v1.11.1 // indirect
-	golang.org/x/text v0.29.0 // indirect
-)
+require github.com/joho/godotenv v1.5.1
+
+require github.com/google/uuid v1.6.0
--- backend/internal/user/repository/repositoryInterface.go
+++ backend/internal/user/repository/repositoryInterface.go
@@ -3,11 +3,11 @@ package repository
 import (
 	"context"
 	"github.com/google/uuid"
-	"userServiseAPI/internal/user/service"
+	"github.com/iiincognito/CRM-detailing-studio/backend/internal/user/service"
 )
 
 type UserRpstr interface {
-	Create(ctx context.Context, user *service.User) error                // Create — создаёт нового пользователя в БД.
+	// Create — создаёт нового пользователя в БД.
 	GetByEmail(ctx context.Context, email string) (*service.User, error) // GetByEmail — ищет пользователя по email.
 	GetByID(ctx context.Context, id uuid.UUID) (*service.User, error)    // GetByID — ищет пользователя по ID.
 	Update(ctx context.Context, user *service.User) error                // Update — обновляет существующие поля пользователя (name, age и т.д.).
